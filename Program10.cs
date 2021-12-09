6834aad8cd62e50e744f41649cefb0378d85e823
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-09 19:00:54 +0200

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()), st = 0, dr = n - 1, mij, k,poz=-1;
+            k = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            mij = (st + dr) / 2;
+            for(int i=0;i<n;i++)
+            {
+                v[i]= int.Parse(Console.ReadLine());
+            }
+           
+              while(st<=dr)
+            {
+                mij = (st + dr) / 2;
+                if(v[mij]==k)
+                {
+                    poz = mij;
+                }
+                else if(v[mij]>k)
+                {
+                    dr = mij - 1;
+                }
+                else if(v[mij]<k)
+                {
+                    st = mij + 1;
+                }
+            }
+           
+                Console.Write("poz");
+        }
+    }
+}
