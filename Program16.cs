fe4ba5e9fe3c62d1b708b31ae396b85993cf4512
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-12 15:08:23 +0200

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()),ok=1;
+            int[] v = new int[n];
+            int nr = 0;
+            v[0] = int.Parse(Console.ReadLine());
+            int x;
+            x = v[0];
+            int cmd=0;
+
+            for(int i=1;i<n;i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                if(ok==1)
+                {
+                    nr = v[i];
+                }
+                else
+                {
+                    x = v[i];
+                }
+                if (x > nr)
+                {
+                    while (nr != 0)
+                    {
+                        int aux = x % nr;
+                        x = nr;
+                        nr = aux;
+                    }
+                    cmd = x;
+                }
+                if (x < nr)
+                {
+                    while (nr != 0)
+                    {
+                        int aux = nr%x;
+                        nr = x;
+                        x= aux;
+                    }
+                    cmd = nr;
+                }
+                if(x==0)
+                {
+                    x = v[i];
+                    ok = 1;
+                }
+                else if(nr==0)
+                {
+                    nr = v[i];
+                    ok = 0;
+                }
+            }
+            Console.WriteLine(cmd);
+        }
+    }
+}
