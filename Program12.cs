424f1960732885499287e722d5d55ee803f41f15
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-12 13:51:07 +0200

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int ok = 0;
+            int[] v = new int[n];
+
+            for(int i=0;i<n;i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            while(ok!=1)
+            {
+                ok = 1;
+
+                for(int i=0;i<n-1;i++)
+                {
+                    if(v[i]>v[i+1])
+                    {
+                        int aux = v[i];
+                        v[i] = v[i + 1];
+                        v[i + 1] = aux;
+                        ok = 0;
+                    }
+                }
+            }
+
+            for(int i=0;i<n;i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+        }
+    }
+}
