6e4ee26d8ac3f9d49e2c749b55e4f4a613f07b03
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-31 11:01:27 +0200

    Add files via upload

---
+++ Program17.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int []v = new int [n];
+            int poz = 0;
+            while(n!=0)
+            {
+                v[poz] = n % b;
+                n = n / b;
+                poz++;
+            }
+            for(int i=poz-1;i>=0;i--)
+            {
+                Console.Write($"{v[i]} ");
+            }
+        }
+    }
+}
