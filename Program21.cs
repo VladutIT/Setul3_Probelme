a86f3bbf3face313486b82955ccdb6b141e389a7
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-31 11:32:59 +0200

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s1 = new int[50];
+            int[] s2= new int[50];
+            int n1, n2,ok=1,i=0,j=0;
+            n1 = int.Parse(Console.ReadLine());
+            for(i=0;i<n1;i++)
+            {
+                s1[i] = int.Parse(Console.ReadLine());
+            }
+            n2 = int.Parse(Console.ReadLine());
+            for (j = 0; j < n2; j++)
+            {
+                s2[j] = int.Parse(Console.ReadLine());
+            }
+            i = 0;
+            j = 0;
+           while(i<n1 || j<n2)
+            {
+                if(s1[i]==s2[j])
+                {
+                    i++;
+                    j++;
+                    ok = 0;
+                }
+               else if(s1[i]<s2[j])
+                {
+                    ok = 2;
+                    break;
+                }
+                else if(s2[j]<s1[i])
+                {
+                    ok = 3;
+                    break;
+                }
+                if(ok==1)
+                {
+                    i++;
+                    j++;
+                }
+            }
+            if(ok==2)
+            {
+                Console.WriteLine("Primul sir este lexicografic");
+            }
+            else if(ok==3)
+            {
+                Console.WriteLine("Al doilea sir este lexicografic");
+            }
+        }
+    }
+}
