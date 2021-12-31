4f57b685bc0e5b95a37b90b0e163970f67c05e30
VladutIT <89774521+VladutIT@users.noreply.github.com>
2021-12-31 11:50:33 +0200

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Setul3_Probelme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v1 = new int[50];
+            int[] v2 = new int[50];
+            int n1, n2, i = 0, j = 0;
+            n1 = int.Parse(Console.ReadLine());
+            int m1 = 0, m2 = 0, minn = 10000, maxx = 0;
+            for (i = 0; i < n1; i++)
+            {
+                v1[i] = int.Parse(Console.ReadLine());
+            }
+            n2 = int.Parse(Console.ReadLine());
+            for (j = 0; j < n2; j++)
+            {
+                v2[j] = int.Parse(Console.ReadLine());
+            }
+            i = 0;
+            j = 0;
+
+            while (i < n1 || j < n2)
+            {
+                if (v1[i] == v2[j])
+                {
+                    Console.WriteLine($"Intersectia este alcatuita din pozitiile: {i} ");
+                }
+                if (minn > v1[i])
+                {
+                    minn = v1[i];
+                }
+                if (minn > v2[i])
+                {
+                    minn = v2[i];
+                }
+                if (maxx < v1[i])
+                {
+                    maxx = v1[i];
+                }
+                if (maxx < v2[i])
+                {
+                    maxx = v2[i];
+                }
+                i++;
+                j++;
+            }
+            Console.WriteLine($"Reuniunea este {minn} {maxx}");
+        }
+    }
+}
